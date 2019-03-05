b4d4f874cff06168f786789e443ed1bd4b45c86d
Ranit Dey <you@example.com>
2019-03-06 01:44:55 +0530

    Updates

---
+++ precache-manifest.caaffd2ff5da7995c0de6fe361597c30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "780bcbc715c88212df47",
+    "url": "/portfolio-react/static/js/runtime~main.780bcbc7.js"
+  },
+  {
+    "revision": "8b1523b66a67378f2e24",
+    "url": "/portfolio-react/static/js/main.8b1523b6.chunk.js"
+  },
+  {
+    "revision": "40ce0b7d1d1debb8705d",
+    "url": "/portfolio-react/static/js/2.40ce0b7d.chunk.js"
+  },
+  {
+    "revision": "8b1523b66a67378f2e24",
+    "url": "/portfolio-react/static/css/main.f5a97f6b.chunk.css"
+  },
+  {
+    "revision": "bcf8fec13f99f3f6996be5efdc9911ea",
+    "url": "/portfolio-react/index.html"
+  }
+];
